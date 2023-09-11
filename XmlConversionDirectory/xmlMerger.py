29dab3147fc83afe4507bc211e49c06670d28e6e
Théo Martinez <theo.martinez-fouche@laas.fr>
2023-09-11 14:13:56 +0200

    stop using get_absolute_path_project function

--- XmlConversionDirectory/xmlMerger.py
+++ XmlConversionDirectory/xmlMerger.py
@@ -1,6 +1,6 @@
 import os
 import xml.etree.ElementTree as ET
-
+import gym_albert_mujoco
 
 def merge_mjcf_files(file_1, file_2, file_name):
     tree_1 = ET.parse(file_1)
@@ -13,22 +13,9 @@ def merge_mjcf_files(file_1, file_2, file_name):
 
     for child in worldbody_2:
         worldbody_1.append(child)
-
-    project_name = "testEnviSim"
-    project_path = get_absolute_path_project(project_name).replace('\\', '/')
+    mujoco_path = gym_albert_mujoco.__file__
+    project_idx = mujoco_path.find("/gym_albert_mujoco")
+    project_path = mujoco_path[:project_idx]
     xml_directory_path = project_path + "/xmlDirectory/"
     link = xml_directory_path + file_name + ".xml"
     tree_1.write(link)
-
-def get_absolute_path_project(project_name):
-
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-
-    current_directory = script_directory
-    while current_directory != os.path.dirname(current_directory):
-        if os.path.basename(current_directory) == project_name:
-            return current_directory
-        current_directory = os.path.dirname(current_directory)
-
-    # If the specified directory is not found, return None
-    return None
